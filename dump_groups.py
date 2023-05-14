c20d87dcbb59a6b578a528a77137a684d0c6f8ad
Dušan Simić <dusan.simic1810@gmail.com>
2023-05-15 00:24:16 +0200

    Add help pages to dump scripts

--- dump_groups.py
+++ dump_groups.py
@@ -1,23 +1,21 @@
 #!/bin/python
 
-# Utility script used for grouping links with their destination files
-# It helps with initial creation of file list for meson build spec
-
 import os
-import sys
+import argparse
 from graphlib import TopologicalSorter
 
-# take reading dir argument (where to look for icons)
-readdir = sys.argv[1] if len(sys.argv) > 1 else '.'
+parser = argparse.ArgumentParser(description='Dump groups of links with their destination files')
+parser.add_argument('readdir', metavar='readdir', type=str, nargs='?', default='.',)
+args = parser.parse_args()
 
 # read all files from directory (expect there are only links to icons)
-files = os.listdir(readdir)
+files = os.listdir(args.readdir)
 
 # filter out link files
 link_files = [
     file
     for file in files
-    if os.path.isfile(os.path.join(readdir, file)) and os.path.islink(os.path.join(readdir, file))
+    if os.path.isfile(os.path.join(args.readdir, file)) and os.path.islink(os.path.join(args.readdir, file))
 ]
 
 # dictonary for grouping files and their links
@@ -25,7 +23,7 @@ groups = {}
 
 # for each link find a file it links to and add it to that group
 for link in link_files:
-    groups.setdefault(os.readlink(os.path.join(readdir, link)), []).append(link)
+    groups.setdefault(os.readlink(os.path.join(args.readdir, link)), []).append(link)
 
 # sort groups topologically
 # that way chain links will be created in correct order
@@ -37,8 +35,8 @@ sorted_groups = [(file, groups[file]) for file in ts.static_order() if file in g
 # reverse the order of groups so first the dependencies are created
 for file, links in reversed(sorted_groups):
     # print a comment line indicating a group of links (file that they link to)
-    print('\'{}\': ['.format(file))
+    print("'{}': [".format(file))
     for link in links:
         # print a line for each link file
-        print('    \'{}\','.format(link))
-    print('],')
+        print("    '{}',".format(link))
+    print("],")
--- dump_regulars.sh
+++ dump_regulars.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+usage() {
+    echo "usage: $0 [directory]"
+    echo "  directory: directory to read from (default: current directory)"
+}
+
 # specify a directory to read from (needs the trailing slash)
 readdir="./"
-if [ -n "$1" ]; then
-    readdir="$1"
+
+if [ "$#" -ge 1 ]; then
+    if [ "$1" = "-h" ]; then
+        usage
+        exit 0
+    fi
+
+    if [ -d "$1" ]; then
+        readdir="$1"
+    else
+        echo "error: $1 is not a directory"
+        usage
+        exit 1
+    fi
 fi
 
 # calculate the number of bytes to cut from
