d3ee80fa8aa6f47627ba24efd2a6420ad1a5f188
Dušan Simić <dusan.simic1810@gmail.com>
2023-05-22 23:52:40 +0200

    Modify dump_groups.py so it populates meson.build

--- _dev/dump_groups.py
+++ _dev/dump_groups.py
@@ -3,13 +3,22 @@
 import os
 import argparse
 from graphlib import TopologicalSorter
+import re
 
-parser = argparse.ArgumentParser(description='Dump groups of links with their destination files')
+parser = argparse.ArgumentParser(description='Update regular files and groups of links in meson.build file.')
 parser.add_argument('readdir', metavar='readdir', type=str, nargs='?', default='.',)
 args = parser.parse_args()
 
-# read all files from directory (expect there are only links to icons)
-files = os.listdir(args.readdir)
+# read all files from directory
+files = sorted(os.listdir(args.readdir))
+files.remove('meson.build')
+
+# filter out regular files
+regular_files = [
+    "    '{}',\n".format(file)
+    for file in files
+    if os.path.isfile(os.path.join(args.readdir, file)) and not os.path.islink(os.path.join(args.readdir, file))
+]
 
 # filter out link files
 link_files = [
@@ -33,10 +42,28 @@ sorted_groups = [(file, groups[file]) for file in ts.static_order() if file in g
 
 # iterate over all groups
 # reverse the order of groups so first the dependencies are created
+new_link_lines = []
 for file, links in reversed(sorted_groups):
     # print a comment line indicating a group of links (file that they link to)
-    print("    '{}': [".format(file))
+    new_link_lines.append("    '{}': [\n".format(file))
     for link in links:
         # print a line for each link file
-        print("        '{}',".format(link))
-    print("    ],")
+        new_link_lines.append("        '{}',\n".format(link))
+    new_link_lines.append("    ],\n")
+
+reg_lines = None
+link_lines = None
+
+# match lines in regular and link segments
+r = re.compile(r'^((?:.*\n)+    # DO NOT REMOVE: Begining of regular segment\n)((?:.+\n)*)(    # DO NOT REMOVE: End of regular segment\n(?:.*\n)+    # DO NOT REMOVE: Begining of link segment\n)((?:.+\n)*)(    # DO NOT REMOVE: End of link segment\n(?:.*\n)+)$', re.MULTILINE)
+
+with open(os.path.join(args.readdir, 'meson.build'), 'r') as f:
+    match_lines = r.match(''.join(f.readlines()))
+
+if match_lines:
+    with open(os.path.join(args.readdir, 'meson.build'), 'w') as f:
+        f.write(match_lines.group(1))
+        f.write(''.join(regular_files))
+        f.write(match_lines.group(3))
+        f.write(''.join(new_link_lines))
+        f.write(match_lines.group(5))
