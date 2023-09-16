bdbc5f3d0908a5fc1e9d97c13092ea6672b48228
AtiusAmy <www.dappertransformer@proton.me>
2023-09-16 15:39:53 +0700

    Added more icons and updated installer
    Cubiomes viewer uwu

--- local-install.sh
+++ local-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p ~/.local/share/icons/MoreWaita/
+mkdir -p ~/.local/share/icons/MoreWaitaPlus/
 shopt -s extglob
-cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ~/.local/share/icons/MoreWaita/
+cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ~/.local/share/icons/MoreWaitaPlus/
 shopt -u extglob
-find ~/.local/share/icons/MoreWaita/ -name '*.build' -type f -delete
-gtk-update-icon-cache -f -t ~/.local/share/icons/MoreWaita && xdg-desktop-menu forceupdate
+find ~/.local/share/icons/MoreWaitaPlus/ -name '*.build' -type f -delete
+gtk-update-icon-cache -f -t ~/.local/share/icons/MoreWaitaPlus && xdg-desktop-menu forceupdate
 
--- system-install.sh
+++ system-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/share/icons/MoreWaita/
+sudo mkdir -p /usr/share/icons/MoreWaitaPlus/
 shopt -s extglob
-sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaita/
+sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaitaPlus/
 shopt -u extglob
-sudo find /usr/share/icons/MoreWaita/ -name '*.build' -type f -delete
-sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita && xdg-desktop-menu forceupdate
+sudo find /usr/share/icons/MoreWaitaPlus/ -name '*.build' -type f -delete
+sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaitaPlus && xdg-desktop-menu forceupdate
