8b41e4bec6514865a03dd6d73a51f734e7484e97
AtiusAmy <www.dappertransformer@proton.me>
2023-09-16 19:26:15 +0700

    undo that for upstream purposes
    gonna try upstreaming?

--- system-install.sh
+++ system-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/share/icons/MoreWaitaPlus/
+sudo mkdir -p /usr/share/icons/MoreWaita/
 shopt -s extglob
-sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaitaPlus/
+sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaita/
 shopt -u extglob
-sudo find /usr/share/icons/MoreWaitaPlus/ -name '*.build' -type f -delete
-sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaitaPlus && xdg-desktop-menu forceupdate
+sudo find /usr/share/icons/MoreWaita/ -name '*.build' -type f -delete
+sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita && xdg-desktop-menu forceupdate
