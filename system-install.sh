307b70bbf6c7b195095f23ef63c19cb4ec372eb5
somepaulo <finomeno@gmail.com>
2023-10-06 02:23:52 +0100

    Maintenance

--- system-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mkdir -p /usr/share/icons/MoreWaita/
-shopt -s extglob
-sudo cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) /usr/share/icons/MoreWaita/
-shopt -u extglob
-sudo find /usr/share/icons/MoreWaita/ -name '*.build' -type f -delete
-sudo gtk-update-icon-cache -f -t /usr/share/icons/MoreWaita && xdg-desktop-menu forceupdate
