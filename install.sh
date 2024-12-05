3e5ff7a2d918a84b34641b54e49e2da8e599fb5c
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-05 09:12:11 +0100

    install.sh: Fix read-only filesystem detection & use proper local system location for icon install
    `-w` bash built-in & greping for `w` in `ls -ld` don't work properly, so try to make a temporary file. If it's made successfully, delete it & return code 1 for `check_read_only` function, otherwise return 0.
    
    `/usr/local/` is a symlink to `/var/usrlocal/` in Fedora Atomic only, so use `/usr/local/` to be more compatible.

--- install.sh
+++ install.sh
@@ -5,20 +5,30 @@ is_user_root ()
 		[ "$(id -u)" -eq 0 ]
 }
 
-if is_user_root; then
-    if ! ls -ld /usr | grep -q 'w'; then
-        THEMEDIR=/var/usrlocal/share/icons/MoreWaita/
+check_read_only() {
+    local dir="${1}"
+    if touch "${dir}/testfilemorewaita" 2>/dev/null; then
+        rm "${dir}/testfilemorewaita"
+	return 1
     else
-        THEMEDIR=/usr/share/icons/MoreWaita/
+        return 0
+    fi
+}
+
+if is_user_root; then
+    if check_read_only /usr; then
+        THEMEDIR="/usr/local/share/icons/MoreWaita/"
+    else
+        THEMEDIR="/usr/share/icons/MoreWaita/"
     fi
 else
-    THEMEDIR=$HOME/.local/share/icons/MoreWaita/
+    THEMEDIR="${HOME}/.local/share/icons/MoreWaita/"
 fi
 
-mkdir -p $THEMEDIR
+mkdir -p ${THEMEDIR}
 shopt -s extglob
-cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) $THEMEDIR
+cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ${THEMEDIR}
 shopt -u extglob
-find $THEMEDIR -name '*.build' -type f -delete
-gtk-update-icon-cache -f -t $THEMEDIR && xdg-desktop-menu forceupdate
+find ${THEMEDIR} -name '*.build' -type f -delete
+gtk-update-icon-cache -f -t ${THEMEDIR} && xdg-desktop-menu forceupdate
 
