bbde4b48179b020caa92efcbc922433f7e323745
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-05 10:00:33 +0100

    chore: Use bash `-w` built-in instead
    It works actually, I just badly tested it.

--- install.sh
+++ install.sh
@@ -5,18 +5,8 @@ is_user_root ()
 		[ "$(id -u)" -eq 0 ]
 }
 
-check_read_only() {
-    local dir="${1}"
-    if touch "${dir}/testfilemorewaita" 2>/dev/null; then
-        rm "${dir}/testfilemorewaita"
-	return 1
-    else
-        return 0
-    fi
-}
-
 if is_user_root; then
-    if check_read_only /usr; then
+    if [ ! -w "/usr/" ]; then
         THEMEDIR="/usr/local/share/icons/MoreWaita/"
     else
         THEMEDIR="/usr/share/icons/MoreWaita/"
@@ -31,4 +21,3 @@ cp -avu "$(pwd -P)"/!(*.build|*.sh|*.py|*.md|.git|.github|.gitignore|_dev) ${THE
 shopt -u extglob
 find ${THEMEDIR} -name '*.build' -type f -delete
 gtk-update-icon-cache -f -t ${THEMEDIR} && xdg-desktop-menu forceupdate
-
